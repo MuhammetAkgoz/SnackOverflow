c4aae36a42dfc165f3aa61cb35467a9155ef87b7
MuhammetAkgoz <muhammet.akgz@gmail.com>
2023-11-30 21:13:14 +0300

    Initial Commit

---
+++ snackoverflow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  snackoverflow
+//
+//  Created by Muhammet Akgöz on 30.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ snackoverflow/snackoverflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  snackoverflowApp.swift
+//  snackoverflow
+//
+//  Created by Muhammet Akgöz on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct snackoverflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
