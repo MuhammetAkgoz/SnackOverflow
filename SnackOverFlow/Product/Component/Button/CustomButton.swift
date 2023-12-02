f85930e4d4ac1ece57216037fb01b91c0dfe0ad5
MuhammetAkgoz <muhammet.akgz@gmail.com>
2023-12-02 22:00:18 +0300

    Onboarding added

---
+++ SnackOverFlow/Product/Component/Button/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  snackoverflow
+//
+//  Created by Muhammet Akgöz on 2.12.2023.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButton()
+}
---
+++ SnackOverFlow/View/Authentication/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  snackoverflow
+//
+//  Created by Muhammet Akgöz on 2.12.2023.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
